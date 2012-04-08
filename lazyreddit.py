5108a09528588af02975f97f0bbb426c268827b9
staticsafe <sadiq.9541@gmail.com>
2012-04-07 23:33:03 -0400

    testing logic

--- lazyreddit.py
+++ lazyreddit.py
@@ -11,8 +11,20 @@ config.read('lazyreddit.cfg')
 email = config.get('main', 'email')
 subreddits = config.get('main', 'subreddits')
 
+# Set user agent as needed
+r = reddit.Reddit(user_agent="lazyreddit")
+
 # parse subreddits further
 
 subreddits = [y.strip().lower() for y in subreddits.split(',')]
 
-print subreddits
+#for subreddits in subreddits:
+#	topten = r.get_subreddit(subreddits[0]).get_top(limit=10)
+
+i = 0
+topten = []
+for i in len(subreddits):
+	topten[i] = r.get_subreddit(subreddits[i].get_top(limit=10))
+	i += 1
+
+print topten[0]
