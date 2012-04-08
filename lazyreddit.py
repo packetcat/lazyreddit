0bd6725b56f40bec66d265ecea01667298c1353f
staticsafe <sadiq.9541@gmail.com>
2012-04-08 00:47:16 -0400

    got something

--- lazyreddit.py
+++ lazyreddit.py
@@ -13,18 +13,13 @@ subreddits = config.get('main', 'subreddits')
 
 # Set user agent as needed
 r = reddit.Reddit(user_agent="lazyreddit")
-
 # parse subreddits further
 
 subreddits = [y.strip().lower() for y in subreddits.split(',')]
 
-#for subreddits in subreddits:
-#	topten = r.get_subreddit(subreddits[0]).get_top(limit=10)
-
 i = 0
-topten = []
-for i in len(subreddits):
-	topten[i] = r.get_subreddit(subreddits[i].get_top(limit=10))
-	i += 1
+for i in range(len(subreddits)):
+    submissions = list(r.get_subreddit(subreddits[i]).get_top(limit=10))
+    i += 1
 
-print topten[0]
+print submissions[1]
